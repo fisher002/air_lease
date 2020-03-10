3e895b7fc6e1652bbf954460733fef63ea074e5d
Junyu <1689074880@qq.com>
2020-03-10 14:33:40 +0800

    项目初步配置

---
+++ src/test/java/com/air/lease/LeaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.air.lease;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
