7e6d3c7995f23512b5fc21754d887ad71055aa09
fisher002 <48755250+fisher002@users.noreply.github.com>
2020-03-26 23:01:58 +0800

    Update LeaseApplication.java

--- src/main/java/com/air/lease/LeaseApplication.java
+++ src/main/java/com/air/lease/LeaseApplication.java
@@ -1,17 +1,11 @@
 package com.air.lease;
 
-<<<<<<< HEAD
-=======
 import org.mybatis.spring.annotation.MapperScan;
->>>>>>> b9d553927d9ad20a99dda1a6f1ea986e056412de
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-<<<<<<< HEAD
-=======
 @MapperScan("com.air.lease.repository") // 包扫描
->>>>>>> b9d553927d9ad20a99dda1a6f1ea986e056412de
 public class LeaseApplication {
 
 	public static void main(String[] args) {
